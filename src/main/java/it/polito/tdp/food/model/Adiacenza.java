e75f812c1c3dd7f2a71da269d6470627178c2bb1
ilarialongo <ilarialongo@mbp-di-ilaria.homenet.telecomitalia.it>
2020-06-22 13:31:52 +0200

    Vertici2

--- src/main/java/it/polito/tdp/food/model/Adiacenza.java
+++ src/main/java/it/polito/tdp/food/model/Adiacenza.java
@@ -1,5 +1,15 @@
 package it.polito.tdp.food.model;
 
 public class Adiacenza {
+	String tipo1;
+	String tipo2;
+	int peso;
+	public Adiacenza(String tipo1, String tipo2, int peso) {
+		super();
+		this.tipo1 = tipo1;
+		this.tipo2 = tipo2;
+		this.peso = peso;
+	}
+	
 
 }
--- src/main/java/it/polito/tdp/food/model/Model.java
+++ src/main/java/it/polito/tdp/food/model/Model.java
@@ -1,17 +1,21 @@
 package it.polito.tdp.food.model;
 
 import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
+import it.polito.tdp.food.db.FoodDAO;
+
 public class Model {
 	private Graph<String, DefaultWeightedEdge> grafo;
+	private FoodDAO dao;
 	public Model() {
-		
+		this.dao=new FoodDAO();	
 	}
 	
 	public void creaGrafo(int C) {
 		this.grafo= new SimpleWeightedGraph<String,DefaultWeightedEdge> (DefaultWeightedEdge.class); 
-		
+		Graphs.addAllVertices(this.grafo, this.dao.getTipologia(C));
 	}
 }
